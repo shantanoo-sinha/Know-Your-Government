2aed415fb82d74524ef482843b7f5c25ef62b8f7
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-10-25 21:09:14 -0500

    Initial commit

---
+++ app/src/main/java/com/shantanoo/know_your_government/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shantanoo.know_your_government;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
